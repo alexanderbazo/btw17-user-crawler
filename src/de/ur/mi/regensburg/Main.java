eeb58b5964ec7e99cee317415496dbfce9ffa90e
blueCat11 <blue.cat@t-online.de>
2017-07-16 17:54:37 +0200

    started adding Bundestagsscraper

--- src/de/ur/mi/regensburg/Main.java
+++ src/de/ur/mi/regensburg/Main.java
@@ -28,13 +28,68 @@ public class Main {
          */
 
     public static void main(String[] args) {
-        ArrayList<String> rows = CSVReader.readCSV(Config.PATH_TO_STRUCTURE_CSV);
+        String path = Config.PATH_TO_STATE_STRUCTURE_CSV;
+
+        ArrayList<String> rows = CSVReader.readCSV(path);
         FileWriter fw = null;
         BufferedWriter bw = null;
         String separator = ";";
+        String fileName = "landtagParliamentMembers.csv";
 
         try {
-            fw = new FileWriter("member.csv");
+            fw = new FileWriter(fileName);
+            bw = new BufferedWriter(fw);
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try{
+            for (int i = 0; i < 16; i++) {
+                Parliament parliament = initParliament(Config.LANDTAG, i);
+
+                ArrayList<ParliamentMember> members = parliament.members(rows.get(i));
+
+                for (int j = 0; j < members.size(); j++) {
+                    ParliamentMember member = members.get(j);
+                    ParliamentMemberCSVWriter.writeLine(bw, member, separator);
+                }
+            }
+
+        }catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                if (bw != null) {
+                    bw.close();
+                }
+                if (fw != null) {
+                    fw.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static Parliament initParliament(int kindOfParliament, int stateOrdinal ) {
+        Parliament parliament;
+        if (kindOfParliament == Config.LANDTAG){
+            if (stateOrdinal == FederalState.BRE.ordinal()) {
+                parliament = new Parliament(Config.PAGES_FOR_STATES[stateOrdinal], "table.sortable", FederalState.values()[stateOrdinal]);
+            } else {
+                parliament = new Parliament(Config.PAGES_FOR_STATES[stateOrdinal], "table.sortable tbody tr", FederalState.values()[stateOrdinal]);
+            }
+        }else{
+            parliament = new Parliament(Config.PAGE_BUNDESTAG, "table.sortable tbody tr");
+        }
+
+        return parliament;
+    }
+
+
+    /*
+    try {
+            fw = new FileWriter(fileName);
             bw = new BufferedWriter(fw);
         }catch (IOException e) {
             e.printStackTrace();
@@ -44,9 +99,9 @@ public class Main {
             for (int i = 0; i < 16; i++) {
                 Parliament parliament;
                 if (i == FederalState.BRE.ordinal()) {
-                    parliament = new Parliament(FederalState.values()[i], "table.sortable");
+                    parliament = new Parliament(Config.PAGES_FOR_STATES[FederalState.values()[i].ordinal()], "table.sortable");
                 } else {
-                    parliament = new Parliament(FederalState.values()[i], "table.sortable tbody tr");
+                    parliament = new Parliament(Config.PAGES_FOR_STATES[FederalState.values()[i].ordinal()], "table.sortable tbody tr");
                 }
 
                 ArrayList<ParliamentMember> members = parliament.members(rows.get(i));
@@ -70,13 +125,7 @@ public class Main {
                 e.printStackTrace();
             }
         }
-    }
+     */
+
 }
 
-        /*//original version
-        Parliament parliament = new Parliament(FederalState.BY);
-        ArrayList<ParliamentMember> members = parliament.members();
-        for(ParliamentMember m: members) {
-            System.out.println(m);
-        }
-        */
